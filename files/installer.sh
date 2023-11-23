13ca0d77fa590b2e27fb1e3bac1008aba6cd3a0e
kithminisg <kithminishanika@gmail.com>
2023-11-23 12:12:35 +0530

    Modified the installer script

--- files/installer.sh
+++ files/installer.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+# This script is also used to install evernode CLI via retrieving the latest release setup tool.
 # Evernode host setup tool to manage Sashimono installation and host registration.
-# This script is also used as the 'evernode' cli alias after the installation.
 # usage: ./installer.sh
 
 # surrounding braces  are needed make the whole script to be buffered on client before execution.
 {
+
+export NETWORK="${NETWORK:-testnet}"
+export NO_DOMAIN="${NO_DOMAIN:-0}"
+export SKIP_SYSREQ="${SKIP_SYSREQ:-0}"
+
 repo_owner="kithminisg"
 repo_name="simulink"
 desired_asset_name="setup.sh"
@@ -18,7 +23,7 @@ download_url=$(echo $release_info | jq -r --arg desired_asset "$desired_asset_na
 # TODO REMOVE NO_DOMAIN
 #curl -fsSL $download_url | cat | sudo bash -s install
 
-curl -fsSL $download_url | cat | sudo NO_DOMAIN=1 bash -s install
+curl -fsSL $download_url | cat | sudo NETWORK=$NETWORK NO_DOMAIN=$NO_DOMAIN SKIP_SYSREQ=$SKIP_SYSREQ bash -s install
 
 # surrounding braces  are needed make the whole script to be buffered on client before execution.
 }
