0c3e6eef6b4e86d372b769c4bc6808608e3beb44
kithminisg <kithminishanika@gmail.com>
2024-05-31 11:58:09 +0530

    reputation and sagent changes

--- sashimono/patches/update-reputationd.sh
+++ sashimono/patches/update-reputationd.sh
@@ -23,7 +23,7 @@ download="/tmp/$file"
 echo "Updating.."
 
 function update() {
-    ! curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/updated-patch/sashimono/patches/resources/reputationd/$file" -o "$download" && echo "Download failed!" && return 1
+    ! curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/updated-patch-kith/sashimono/patches/resources/reputationd/$file" -o "$download" && echo "Download failed!" && return 1
     ! mkdir $REPUTATIOND_BIN && echo "Directory creation failed!" && return 1
     ! tar -xf "$download" -C "$REPUTATIOND_BIN" && echo "Unzip failed!" && return 1
     ! chmod +x "$REPUTATIOND_BIN" && echo "Ownership change failed!" && return 1
@@ -33,7 +33,7 @@ function update() {
 if (! update); then
     echo "Update failed. Restoring.."
     rm -r "$REPUTATIOND_BIN"
-    ! cp "$backup" "$REPUTATIOND_BIN" && echo "Restoring failed." && exit 1
+    ! cp -Rdp "$backup" "$REPUTATIOND_BIN" && echo "Restoring failed." && exit 1
     echo "Restored."
     exit 1
 fi
--- sashimono/patches/update-sashimono.sh
+++ sashimono/patches/update-sashimono.sh
@@ -19,7 +19,7 @@ mv "$SASHIMONO_BIN/$file" "$backup_file"
 
 echo "Updating the files.."
 
-if (! curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/updated-patch/sashimono/patches/resources/sashimono/$file" -o "$SASHIMONO_BIN/$file") || (! chmod +x "$SASHIMONO_BIN/$file"); then
+if (! curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/updated-patch-kith/sashimono/patches/resources/sashimono/$file" -o "$SASHIMONO_BIN/$file") || (! chmod +x "$SASHIMONO_BIN/$file"); then
     echo "Update failed. Restoring.."
     ! cp "$backup_file" "$SASHIMONO_BIN/$file" && echo "Restoring failed." && exit 1
     echo "Restored."
