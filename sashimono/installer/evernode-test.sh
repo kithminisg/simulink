0897174fa454ecaa41c80409fa71654b3ab093f9
Kithmini Gunawardhana <kithminishanika@gmail.com>
2024-10-22 11:30:10 +0530

    Update evernode-test.sh

--- sashimono/installer/evernode-test.sh
+++ sashimono/installer/evernode-test.sh
@@ -39,4 +39,4 @@ fi
 echo "Found Sashimono $VERSION..."
 
 # Exucute the setup
-curl -fsSL $setup | cat | sudo NETWORK=devnet OVERRIDE_EVERNODE_GOVERNOR_ADDRESS=r9DFqbCVyNkA5HBog1hME9iSdg9FnbXgnV SKIP_SYSREQ=1 NO_DOMAIN=1 bash -s ${@}
+curl -fsSL $setup | cat | sudo NETWORK=devnet OVERRIDE_EVERNODE_GOVERNOR_ADDRESS=rfzSKmhfSpV3PkHSkMsrr6WN18kJ7sipUR SKIP_SYSREQ=1 NO_DOMAIN=1 bash -s ${@}
